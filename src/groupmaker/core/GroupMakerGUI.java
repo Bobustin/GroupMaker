c874348259a4d8fc97a4a63f742a0b395ffaf677
Carr.Drew.s631223 <s631223@rrisd.local>
2016-04-15 13:17:38 -0500

    Base

--- src/groupmaker/core/GroupMakerGUI.java
+++ src/groupmaker/core/GroupMakerGUI.java
@@ -1,5 +1,21 @@
 package groupmaker.core;
 
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class GroupMakerGUI {
 
+	JFrame frame = new JFrame();
+	JPanel selectionPanel = new JPanel();
+	JLabel test = new JLabel("Test");
+	public GroupMakerGUI()
+	{
+		frame.setVisible(true);
+		frame.setTitle("Group Maker");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		selectionPanel.add(test);
+		frame.add(selectionPanel);
+	}
+	
 }
