4ce44827993a52125c79b489c83495cd707c59ec
Caleb Wilson <lodeous@gmail.com>
2016-04-15 13:08:28 -0500

    added io code

--- src/groupmaker/core/Runner.java
+++ src/groupmaker/core/Runner.java
@@ -1,9 +1,23 @@
 package groupmaker.core;
 
+import java.util.ArrayList;
+import java.io.*;
+
 public class Runner 
 {
 	public static void main(String args[])
-		{
+	{
 			
+	}
+	
+	public ArrayList<String> getStudentNames(String filepath) throws IOException{
+		ArrayList<String> names = new ArrayList<String>();		
+		BufferedReader reader = new BufferedReader(new FileReader(filepath));
+		String line;
+		while((line=reader.readLine())!=null){
+			names.add(line);
 		}
+		reader.close();
+		return names;
+	}
 }
